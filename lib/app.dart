2042645fc36fee38008e583e03fa4e339c7fc2ad
sayodaa <sayedmatger@gmail.com>
2025-04-21 02:57:03 +0200

    handle app

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'E-Commerce',
+      
+      );
+  }
+}
